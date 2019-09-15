de73d6a95eb1a2dea50c44bb478339362ee20447
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-09-14 20:34:10 -0400

    Configure Suyi

---
+++ Farming Prototype/Assets/Scripts/Suyi/MonsterBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonsterBase : MonoBehaviour
+{
+	protected virtual void Awake()
+	{
+	}
+
+	protected virtual void Update()
+	{
+	}
+}
